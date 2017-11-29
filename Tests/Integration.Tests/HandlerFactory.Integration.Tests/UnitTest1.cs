3f0d851f2f3b185d2919fe5913536fecb934aa8a
David Indrawes <david.indrawes@gmail.com>
2017-11-29 15:55:14 +0000

    adding unit, Integration and acceptance tests

---
+++ Tests/Integration.Tests/HandlerFactory.Integration.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HandlerFactory.Integration.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
