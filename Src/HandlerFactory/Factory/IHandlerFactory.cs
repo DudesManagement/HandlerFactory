74df707cea4bd468e2ac97cafef4ead073568815
David Indrawes <david.indrawes@gmail.com>
2017-12-05 10:56:46 +0000

    adding default and null factory implementation

---
+++ Src/HandlerFactory/Factory/IHandlerFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dudes.HandlerFactory.Factory
+{
+    interface IHandlerFactory <T>
+    {
+        /// <summary>
+        /// Resolve all the appropriate handler
+        /// </summary>
+        /// <returns> The corresponding handler object </returns>
+        T Resolve();
+
+        /// <summary>
+        /// Resolve all the appropriate handler, given the packageID and the feed source
+        /// </summary>
+        /// <param name="packageID"> Name of the package to use </param>
+        /// <param name="feedSource"> The url or the local path where you can find the packages repository to pull the package from </param>
+        /// <returns> The corresponding handler object </returns>
+        T Resolve(string packageID, Uri feedSource);
+
+        /// <summary>
+        /// Resolve all the appropriate handler, given the packageID, the feed source and the default namespace
+        /// </summary>
+        /// <param name="packageID"> Name of the package to use </param>
+        /// <param name="feedSource"> The url or the local path where you can find the packages repository to pull the package from </param>
+        /// <param name="handlerFullNameSpace"> The full namespace where we can find the handler </param>
+        /// <returns> The corresponding handler object </returns>
+        T Resolve(string packageID, Uri feedSource, string handlerFullNameSpace);
+    }
+}
