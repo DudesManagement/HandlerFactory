21a6b6a23ee15b3e4c7a486fc2ec392e0fd78587
David Indrawes <david.indrawes@gmail.com>
2018-02-12 09:51:18 +0000

    renaming packageprovider to packageSearcher makes more sense

---
+++ Src/HandlerFactory/PackageSearcher/IPackageSearcher.cs
@@ -0,0 +1,25 @@
+﻿using NuGet.Protocol.Core.Types;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Horus.HandlerFactory.PackageSearcher
+{
+    internal interface IPackageSearcher
+    {
+        /// <summary>
+        /// search for packages with given package ID, and return list of the packages found metadata
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<IPackageSearchMetadata>> GetPackagesMetaDataAsync();
+
+        /// <summary>
+        /// search for packages with given package ID, and return list of the packages found metadata
+        /// can pass a filter to it to be applied during the search
+        /// </summary>
+        /// <param name="searchFilter"></param>
+        /// <returns></returns>
+        Task<IEnumerable<IPackageSearchMetadata>> GetPackagesMetaDataFullSearchAsync(SearchFilter searchFilter);
+    }
+}
